37a014c786e142049b8ec7ae145e2b0808448a23
clemencetbk <kohclemence@gmail.com>
2019-11-05 21:55:49 -0500

    Board struct with bitboards

---
+++ src/board.rs
@@ -0,0 +1,16 @@
+// A collection of bitboards for each piece type/colour
+pub struct Board {
+    u64 WhitePawn;
+    u64 WhiteRook;
+    u64 WhiteKnight;
+    u64 WhiteBishop;
+    u64 WhiteKing;
+    u64 WhiteQueen;
+    
+    u64 BlackPawn;
+    u64 BlackRook;
+    u64 BlackKnight;
+    u64 BlackBishop;
+    u64 BlackKing;
+    u64 BlackQueen;
+};
