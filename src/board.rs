6a003ef6a27a3c896d56663dbc453b9ef98f576a
clemencetbk <kohclemence@gmail.com>
2019-11-06 23:32:59 -0500

    Add new() for board

--- src/board.rs
+++ src/board.rs
@@ -14,3 +14,22 @@ pub struct Board {
     pub black_king: u64,
     pub black_queen: u64
 }
+
+pub fn new() -> Board {       
+    Board {
+        white_pawn: (0b11111111 << 8),
+        white_rook: 0b10000001,
+        white_knight: 0b01000010,
+        white_bishop: 0b00100100,
+        white_king: 0b00010000,
+        white_queen: 0b00001000,
+        
+        black_pawn: (0b11111111 << 50),
+        black_rook: (0b10000001 << 58),
+        black_knight: (0b01000010 << 58),
+        black_bishop: (0b00100100 << 58), 
+        black_king: (0b00010000 << 58),
+        black_queen: (0b00001000 << 58),
+
+    }
+}
