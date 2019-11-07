e404667cf25df40c8b2fd2ef4d2d92fceb0502df
clemencetbk <kohclemence@gmail.com>
2019-11-06 23:30:56 -0500

    Fix declaration of Board struct

--- src/board.rs
+++ src/board.rs
@@ -1,16 +1,16 @@
 // A collection of bitboards for each piece type/colour
 pub struct Board {
-    u64 WhitePawn;
-    u64 WhiteRook;
-    u64 WhiteKnight;
-    u64 WhiteBishop;
-    u64 WhiteKing;
-    u64 WhiteQueen;
+    pub white_pawn: u64,
+    pub white_rook: u64,
+    pub white_knight: u64,
+    pub white_bishop: u64,
+    pub white_king: u64,
+    pub white_queen: u64,
     
-    u64 BlackPawn;
-    u64 BlackRook;
-    u64 BlackKnight;
-    u64 BlackBishop;
-    u64 BlackKing;
-    u64 BlackQueen;
-};
+    pub black_pawn: u64,
+    pub black_rook: u64,
+    pub black_knight: u64,
+    pub black_bishop: u64,
+    pub black_king: u64,
+    pub black_queen: u64
+}
