4b2546e0b84f3cc90eab291ac6d7cd9662f3a911
clemencetbk <kohclemence@gmail.com>
2019-11-12 23:54:44 -0500

    Read args one at a time instead of whole line at once

--- src/uci.rs
+++ src/uci.rs
@@ -1,28 +1,22 @@
 use std::io::{self, Read, Write, stdin, stdout};
+use std::io::BufRead;
 
 pub fn uci_loop() -> io::Result<()> {
     loop {
-        let input_str = read();
-        let input_slice: Vec<&str> = input_str.as_slice()
-                               .iter()
-                               .map(|x| x.as_str())
-                               .collect(); 
-        match input_slice.as_slice() {
-            ["uci", ..] => { 
+        let command = read_to_space();
+        match command.as_ref() {
+            "uci" => { 
                 println!("id\noption\nuciok\n");
                 // TODO: add engine info and options
             }
-            ["debug", "on"] => continue, // TODO: implement debug mode
-            ["debug", "off"] => continue, // TODO: implement debug mode
-            ["isready", ..] => println!("isready"),
-            ["setoption", "name", "button", ..] => continue,
-            ["setoption", "name", name, "value", value] => continue,
-            ["register", "later", ..] => continue,
-            ["register", "name", name, "code", code, ..] => continue,
-            ["ucinewgame", ..] => continue,
-            ["position", ..] => continue,
+            "debug" => continue, // TODO: implement debug mode
+            "isready" => println!("isready"),
+            "setoption" => setoption(),
+            "register" => continue,
+            "ucinewgame" => continue,
+            "position" => continue,
             // * position [fen  | startpos ]  moves  .... 
-            ["go", ..] => continue,
+            "go" => continue,
 	        // * searchmoves  .... 
 	        // * ponder
 	        // * wtime 
@@ -35,14 +29,20 @@ pub fn uci_loop() -> io::Result<()> {
 	        // * mate 
 	        // * movetime 
 	        // * infinite
-            ["stop", ..] => continue,
-            ["ponderhit", ..] => continue,
-            ["quit", ..] => break,
+            "stop" => continue,
+            "ponderhit" => continue,
+            "quit" => break,
             _ => continue,
         }
     }
     Ok(())
 }   
+
+pub fn setoption() {
+    // Read tokens from stdin. Handle cases:
+    // ["setoption", "name", name, "value", value]
+    // ["setoption", "name", "button"] -- no value needed
+}
 
 pub fn read() -> Vec<String> {
     let mut input = String::new();
@@ -51,4 +51,13 @@ pub fn read() -> Vec<String> {
         .expect("Error: could not read input");
     let split = input.split_whitespace();
     split.map(String::from).collect()
+}
+
+// Only read input until first whitespace
+pub fn read_to_space() -> String {
+    let mut buf = vec![];
+    stdin().lock()
+        .read_until(b' ', &mut buf)
+        .expect("Error: could not read input");
+    String::from_utf8_lossy(&buf).to_string()
 }
