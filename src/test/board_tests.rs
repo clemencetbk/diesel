5151d3d35f4176927efffcebd8afa12daad5f63d
clemencetbk <kohclemence@gmail.com>
2020-01-03 15:43:16 -0500

    Add test_fen_few()

--- src/test/board_tests.rs
+++ src/test/board_tests.rs
@@ -13,7 +13,22 @@ fn test_fen_empty() {
     let init = String::from("8/8/8/8/8/8/8/8 w KQkq - 0 1");
     from_fen(&init, &mut b);
     for i in 0..14 {
-        println!("{}, {}", i, format!("{:b}", b.bitboards[i]));
         assert_eq!(b.bitboards[i], 0);
     }
+}
+
+#[test]
+fn test_fen_few() {
+    let mut b = Board::new();
+    let init = String::from("r7/8/8/8/8/8/8/1N3Q2 w KQkq - 0 1");
+    from_fen(&init, &mut b);
+    for i in 0..14 {
+        if (i != ROOK | BLACK) && (i != KNIGHT | WHITE) && (i != QUEEN | WHITE) {
+            println!("{}", i);
+            assert_eq!(b.bitboards[i], 0);
+        }
+    }
+    assert_eq!(b.bitboards[ROOK | BLACK], 1);
+    assert_eq!(b.bitboards[KNIGHT | WHITE], 1 << 57);
+    assert_eq!(b.bitboards[QUEEN | WHITE], 1 << 61);
 }
